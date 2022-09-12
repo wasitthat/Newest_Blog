c36ab8afd302c7cafa94ae750a3ce12bf1308561
John Oden <84195301+wasitthat@users.noreply.github.com>
2022-09-11 20:36:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sqlalchemy import ForeignKey
 import os
 is_admin = False
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('NEWEST_BLOG_SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
